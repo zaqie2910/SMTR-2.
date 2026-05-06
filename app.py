53ebb43444b9537694954940533bf5418be55bce
Azzam Muzaqie <zaqie2910@gmail.com>
2026-05-06 20:10:17 +0700

    new

--- app.py
+++ app.py
@@ -21,3 +21,19 @@ print("hello wrod")
 # matikan local server: ctrl + c
 # buat app blog
 # >>> python manage.py startapp blog
+
+# …or create a new repository on the command line
+
+# echo "# SMTR-2." >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/zaqie2910/SMTR-2..git
+# git push -u origin main
+
+# …or push an existing repository from the command line
+
+# git remote add origin https://github.com/zaqie2910/SMTR-2..git
+# git branch -M main
+# git push -u origin main
